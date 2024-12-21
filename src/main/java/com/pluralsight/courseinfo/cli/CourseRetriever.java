8a435cab76b90bdca8fc8af23c35ea4035846fd1
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2024-12-21 13:23:39 +0000

    Start Project

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,2 +1,28 @@
-package com.pluralsight.courseinfo.cli;public class CourseRetriever {
+package com.pluralsight.courseinfo.cli;
+
+public class CourseRetriever {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+        if(args.length == 0){
+            System.out.println("Please enter an author name as a first argument.");
+            return;
+        }
+        try {
+            retrieveCourse(args[0]);
+        } catch (Exception ex) {
+            System.out.println("Unexpected Error!");
+            ex.printStackTrace();
+        }
+
+
+
+    }
+
+    private static void retrieveCourse(String authorId) {
+
+        System.out.println("Retrieving course for author " + authorId);
+
+    }
 }
