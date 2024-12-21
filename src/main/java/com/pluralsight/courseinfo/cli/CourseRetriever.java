23aef03ab05c61a868ca6454c023165fde8e3d93
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2024-12-21 13:50:13 +0000

    added few codes

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,28 +1,27 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        // LOG.info -> is a version of System.ou.println in slf4j API
+        LOG.info("Hello");
 
         if(args.length == 0){
-            System.out.println("Please enter an author name as a first argument.");
+            LOG.warn("Please enter an author name as a first argument.");
             return;
         }
         try {
             retrieveCourse(args[0]);
         } catch (Exception ex) {
-            System.out.println("Unexpected Error!");
-            ex.printStackTrace();
+            LOG.error("Unexpected Error!", ex);
         }
-
-
-
     }
 
     private static void retrieveCourse(String authorId) {
-
-        System.out.println("Retrieving course for author " + authorId);
-
+        LOG.info("Retrieving course for author '{}'", authorId);
     }
 }
